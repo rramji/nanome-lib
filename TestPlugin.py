5033c106076941f9087e76a00d64344048eb2785
Ethan Vander Horn <ethan.vanderh123@gmail.com>
2019-05-01 13:35:50 -0700

    modified test plugin

--- TestPlugin.py
+++ TestPlugin.py
@@ -1,12 +1,26 @@
 import nanome
 from timeit import default_timer as timer
-
+import os
 import sys, inspect
 import test_plugins
+import sys
+import subprocess
+
+def runplugin (plugin_class):
+    plugin = nanome.Plugin("Testing Plugin", "Plugin is being run using the tester plugin.", "Test", True)
+    plugin.set_plugin_class(plugin_class)
+    plugin.run('127.0.0.1', 8888)
+
 if __name__ == "__main__":
-    class_name = sys.argv[1]
+    class_name = str(sys.argv[1])
+    pluginNum = int(sys.argv[2])
+    if (pluginNum >= 0):
+        subprocess.Popen(["python", sys.argv[0], class_name, str(pluginNum-1)])
+        del sys.argv[2]
+        del sys.argv[1]
+    else:
+        exit()
     module_name = "test_plugins." + class_name
-    del sys.argv[1]
     if module_name not in sys.modules:
         raise "No module: " + module_name
     clsmembers = inspect.getmembers(sys.modules[module_name], inspect.isclass)
@@ -17,6 +31,4 @@ if __name__ == "__main__":
             break
     if plugin_class == None:
         raise "No class " + class_name + " in module " + module_name
-    plugin = nanome.Plugin("Testing Plugin", "Plugin is being run using the tester plugin.", "Test", True)
-    plugin.set_plugin_class(plugin_class)
-    plugin.run('127.0.0.1', 8888)
+    runplugin(plugin_class)
