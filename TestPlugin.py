2dcd9dbd359ea13aff4a63ebbb092644f44d6182
Ethan Vander Horn <ethan.vanderh123@gmail.com>
2019-05-06 13:04:14 -0700

    no message

--- TestPlugin.py
+++ TestPlugin.py
@@ -7,14 +7,14 @@ import sys
 import subprocess
 
 def runplugin (plugin_class):
-    plugin = nanome.Plugin("Testing Plugin", "Plugin is being run using the tester plugin.", "Test", True)
+    plugin = nanome.Plugin("✏Testing Plugin", "Plugin is being run using the tester plugin.", "Test", True)
     plugin.set_plugin_class(plugin_class)
     plugin.run('127.0.0.1', 8888)
 
 if __name__ == "__main__":
     class_name = str(sys.argv[1])
     pluginNum = int(sys.argv[2])
-    if (pluginNum >= 0):
+    if (pluginNum > 0):
         subprocess.Popen(["python", sys.argv[0], class_name, str(pluginNum-1)])
         del sys.argv[2]
         del sys.argv[1]
@@ -22,7 +22,7 @@ if __name__ == "__main__":
         exit()
     module_name = "test_plugins." + class_name
     if module_name not in sys.modules:
-        raise "No module: " + module_name
+        raise Exception("No module: " + module_name)
     clsmembers = inspect.getmembers(sys.modules[module_name], inspect.isclass)
     plugin_class = None
     for clsmember in clsmembers:
